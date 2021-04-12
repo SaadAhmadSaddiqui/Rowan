d6d17cbbe586dba0e99f1bf4371a4c7973361ed6
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2021-04-12 15:15:39 +0400

    added the msgs with animations

--- src/app/components/SectionOne/section-one/section-one.component.ts
+++ src/app/components/SectionOne/section-one/section-one.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-section-one',
   templateUrl: './section-one.component.html',
-  styleUrls: ['./section-one.component.scss']
+  styleUrls: ['./section-one.component.scss'],
 })
 export class SectionOneComponent implements OnInit {
+  @ViewChild('#navbar') navbar!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
