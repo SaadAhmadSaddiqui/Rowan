f7c7a1bd6b6193161b47baea3907ed819e1e517d
Saad Ahmad Saddiqui <mehunsaadi@gmail.com>
2021-04-18 12:28:54 +0400

    font changes to section one

--- src/app/components/homepage/SectionTwo/steppart/steppart.component.ts
+++ src/app/components/homepage/SectionTwo/steppart/steppart.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import SwiperCore, {
+  Navigation,
+  Autoplay
+} from 'swiper/core';
+
+SwiperCore.use([Navigation, Autoplay]);
+
+
 @Component({
   selector: 'section-two-steppart',
   templateUrl: './steppart.component.html',
@@ -12,6 +20,29 @@ export class SteppartComponent implements OnInit {
     'Rowan gets to work',
     'Receive your refund',
   ];
+
+    breakpoints = {
+        // when window width is >= 300px
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 420px
+        420: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 720px
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 10
+        }
+    }
 
   constructor() {}
 
